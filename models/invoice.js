cf0edfc69e39d8086d9082f1d140cd8dae4369e3
FTeddy <ainsophoure@gmail.com>
2018-02-28 19:03:47 +0700

    unactive invoice associations added

--- models/invoice.js
+++ models/invoice.js
@@ -18,6 +18,9 @@ module.exports = (sequelize, DataTypes) => {
   });
   Invoice.associate = function(models) {
     // associations can be defined here
+    // Invoice.belongsTo(models.Costumer)
+    // Invoice.belongsToMany(models.Menu, {through : 'InvoiceMenu'})
+    // Invoice.hasMany(models.InvoiceMenu)
   };
   return Invoice;
 };
