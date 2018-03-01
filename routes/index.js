0d77214c3fb2dbe77a2f3079d7c120c6b78a2c4e
FTeddy <ainsophoure@gmail.com>
2018-03-01 13:34:05 +0700

    added transaction date display on ejs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Controller = require('../controller/index.js').Controller
 
-router.use('/menu', require('./menu.js'))
+// router.use('/menu', require('./menu.js'))
 router.use('/costumer', require('./costumer.js'))
 router.use('/invoice', require('./invoice.js'))
 
